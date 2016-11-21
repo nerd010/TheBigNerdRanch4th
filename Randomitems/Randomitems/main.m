b16d78bb729a75de39d3704ac621578042e279fb
nerd010 <markwang559@gmail.com>
2016-11-21 16:58:44 +0800

    Randomitems

---
+++ Randomitems/Randomitems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Randomitems
+//
+//  Created by Richard Wang on 21/11/2016.
+//  Copyright © 2016 Richard Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
