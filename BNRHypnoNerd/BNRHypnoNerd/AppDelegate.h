14c7abc3dc6513da12acf823e1c0e0d75eed5026
Richard <xieyu559@163.com>
2016-11-26 17:47:58 +0800

    创建 BNRHypnoNerd 项目

---
+++ BNRHypnoNerd/BNRHypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BNRHypnoNerd
+//
+//  Created by Richard Wang on 2016/11/26.
+//  Copyright © 2016年 Richard Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BNRHypnoNerd/BNRHypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BNRHypnoNerd
+//
+//  Created by Richard Wang on 2016/11/26.
+//  Copyright © 2016年 Richard Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
