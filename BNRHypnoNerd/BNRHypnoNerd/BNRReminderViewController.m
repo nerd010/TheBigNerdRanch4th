e87317be1ce4ae17d8a6a645e6192c7689d55aa1
Richard <xieyu559@163.com>
2016-11-27 18:53:37 +0800

    添加本地通知

--- BNRHypnoNerd/BNRHypnoNerd/BNRReminderViewController.m
+++ BNRHypnoNerd/BNRHypnoNerd/BNRReminderViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BNRReminderViewController.h"
+#import <UserNotifications/UserNotifications.h>
 
 @interface BNRReminderViewController ()
 
@@ -35,6 +36,32 @@
     NSDate *date = self.datePicker.date;
     NSString *dateStr = [datef stringFromDate:date];
     NSLog(@"Setting a reminder for %@", dateStr);
+    
+//    UILocalNotification *note = [[UILocalNotification alloc] init];
+//    note.alertBody = @"Hypnotize me!";
+//    note.fireDate = date;
+//    [[UIApplication sharedApplication] scheduleLocalNotification:note];
+    
+    [self registerNotification:30];
 }
 
+- (void)registerNotification:(NSInteger )alerTime
+{
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    content.title = [NSString localizedUserNotificationStringForKey:@"Hello!" arguments:nil];
+    content.body = [NSString localizedUserNotificationStringForKey:@"Hypnotize me!" arguments:nil];
+    content.sound = [UNNotificationSound defaultSound];
+    
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:alerTime repeats:NO];
+    UNNotificationRequest* request = [UNNotificationRequest requestWithIdentifier:@"FiveSecond"
+                                                                          content:content trigger:trigger];
+    //添加推送成功后的处理！
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"本地通知" message:@"成功添加推送" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+        [alert addAction:cancelAction];
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+    }];
+}
 @end
