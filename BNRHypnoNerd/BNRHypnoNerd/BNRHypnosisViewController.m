d3104e88e624abe2ce7f57bb418a0c4864fe3330
Richard <xieyu559@163.com>
2016-11-28 23:38:47 +0800

    添加20个 随机生成的Label

--- BNRHypnoNerd/BNRHypnoNerd/BNRHypnosisViewController.m
+++ BNRHypnoNerd/BNRHypnoNerd/BNRHypnosisViewController.m
@@ -42,9 +42,35 @@
     self.view = backgroundView;
 }
 
+- (void)drawHynoticMessage:(NSString *)message
+{
+    for (int i = 0; i < 20; i++)
+    {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+        
+        [messageLabel sizeToFit];
+        
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+        
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        [self.view addSubview:messageLabel];
+    }
+}
+
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    NSLog(@"textField:%@", textField);
+//    NSLog(@"textField:%@", textField);
+    [self drawHynoticMessage:textField.text];
+    textField.text = @"";
     [textField resignFirstResponder];
     return YES;
 }
