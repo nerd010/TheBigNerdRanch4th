8a2823878c6c229277f29e763d9c2da34fa30aa5
Richard <xieyu559@163.com>
2016-11-30 23:09:47 +0800

    BNRHomepwner

---
+++ BNRHomepwner/BNRHomepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BNRHomepwner
+//
+//  Created by Richard Wang on 2016/11/30.
+//  Copyright © 2016年 Richard Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BNRHomepwner/BNRHomepwner/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BNRHomepwner
+//
+//  Created by Richard Wang on 2016/11/30.
+//  Copyright © 2016年 Richard Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
