6d7192d6684802569f2dbdba05e8027f345efb05
Richard <xieyu559@163.com>
2016-12-06 12:03:46 +0800

    设置 imageStore

--- BNRHomepwner/BNRHomepwner/BNRImageStore.h
+++ BNRHomepwner/BNRHomepwner/BNRImageStore.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface BNRImageStore : NSObject
+
++ (instancetype)sharedStore;
+
+- (void)setImage:(UIImage *)image forKey:(NSString *)key;
+- (UIImage *)imageForKey:(NSString *)key;
+- (void)deleteImageForKey:(NSString *)key;
 
 @end
