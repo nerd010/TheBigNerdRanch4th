cbee645e746c2eff2bdd86e536fa68d19903cf0d
Richard <xieyu559@163.com>
2016-12-06 11:40:05 +0800

    创建 BNRImageStore

---
+++ BNRHomepwner/BNRHomepwner/BNRImageStore.h
@@ -0,0 +1,13 @@
+//
+//  BNRImageStore.h
+//  BNRHomepwner
+//
+//  Created by Richard Wang on 06/12/2016.
+//  Copyright © 2016 Richard Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRImageStore : NSObject
+
+@end
---
+++ BNRHomepwner/BNRHomepwner/BNRImageStore.m
@@ -0,0 +1,13 @@
+//
+//  BNRImageStore.m
+//  BNRHomepwner
+//
+//  Created by Richard Wang on 06/12/2016.
+//  Copyright © 2016 Richard Wang. All rights reserved.
+//
+
+#import "BNRImageStore.h"
+
+@implementation BNRImageStore
+
+@end
