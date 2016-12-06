361546d1bc5d82fa36aa2ac4beacc2ca7072fec3
Richard <xieyu559@163.com>
2016-12-06 19:31:07 +0800

    绘图功能代码

--- BNRTouchTracker/BNRTouchTracker/BNRDeawView.m
+++ BNRTouchTracker/BNRTouchTracker/BNRDeawView.m
@@ -7,7 +7,53 @@
 //
 
 #import "BNRDeawView.h"
+#import "BNRLine.h"
+
+@interface BNRDeawView ()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation BNRDeawView
+
+- (instancetype)initWithFrame:(CGRect)r
+{
+    self = [super initWithFrame:r];
+    if (self) {
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    //用黑色绘制已经完成的线条
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines)
+    {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine)
+    {
+        //用红色绘制正在画的线条
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
+}
 
 @end
