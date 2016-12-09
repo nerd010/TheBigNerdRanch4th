c28b585df93cbf765c80c32fc4e50e67172b2858
Richard <xieyu559@163.com>
2016-12-09 19:32:56 +0800

    添加 Tap 手势

--- BNRTouchTracker/BNRTouchTracker/BNRDeawView.m
+++ BNRTouchTracker/BNRTouchTracker/BNRDeawView.m
@@ -26,6 +26,10 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        [self addGestureRecognizer:doubleTapRecognizer];
     }
     return self;
 }
@@ -115,6 +119,14 @@
         [self.linesInProgress removeObjectForKey:key];
     }
     
+    [self setNeedsDisplay];
+}
+
+- (void)doubleTap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Double Tap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
     [self setNeedsDisplay];
 }
 @end
