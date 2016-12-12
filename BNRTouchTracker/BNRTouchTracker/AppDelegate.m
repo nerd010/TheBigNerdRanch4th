3ad470bd115bdcdab4c3b993280b136c748960c3
Richard <xieyu559@163.com>
2016-12-12 14:43:26 +0800

    添加调试模式

--- BNRTouchTracker/BNRTouchTracker/AppDelegate.m
+++ BNRTouchTracker/BNRTouchTracker/AppDelegate.m
@@ -23,6 +23,9 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
+#ifdef VIEW_DEBUG
+    NSLog(@"====111====%@", [self.window performSelector:@selector(recursiveDescription)]);
+#endif
     return YES;
 }
 
