e2d2a7c163579a17d002e80d2fe147b941af0123
Richard <xieyu559@163.com>
2016-12-29 22:25:25 +0800

    动画

--- BNRHypnoNerd/BNRHypnoNerd/BNRHypnosisViewController.m
+++ BNRHypnoNerd/BNRHypnoNerd/BNRHypnosisViewController.m
@@ -63,6 +63,33 @@
         messageLabel.frame = frame;
         
         [self.view addSubview:messageLabel];
+        
+        // 设置 messageLabel 透明度的起始值
+        messageLabel.alpha = 0.0;
+        // 透明度由0.0变为1.0
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseIn
+                         animations:^{
+                             messageLabel.alpha = 1.0;
+                         }
+                         completion:NULL];
+        
+        [UIView animateKeyframesWithDuration:1.0 delay:0.0 options:0 animations:^{
+            [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.8 animations:^{
+                messageLabel.center = self.view.center;
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.8 animations:^{
+                int x = arc4random() % width;
+                int y = arc4random() % height;
+                messageLabel.center = CGPointMake(x, y);
+            }];
+        } completion:^(BOOL finished){
+            NSLog(@"Animation finished");
+        }];
+        
+        // 随着设备在空间的移动而发生微移
         UIInterpolatingMotionEffect *motionEffect;
         motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x"
                                                                        type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
